d9d49cf83a39bc6034ffdc8e3c7ac3be7eccbbae
Cristian <jejecristian@gmail.com>
2022-05-17 05:50:10 -0400

    Proyecto terminado
    Proyecto terminado incorpora html5, css, bootstrap, fontawesome, googlefonts, y jquery.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
 /** Se prepara script */
+$(document).ready(function () {
+
+    // Aplica efecto smooth scroll
+    $('a').click(function () {
+        var itemPress = this.hash
+        $('html, body').animate(
+            {
+                scrollTop: $(itemPress).offset().top - 40
+            }, 800
+        )
+    })
+
+    /** Esconde/Muestra botones del carrusel
+    cuando se presiona el boton hamburguesa */
+    $('.navbar-toggler').click(function(){
+        $('#principal button').toggle()
+    })
+
+})
+
+// Bootstrap - Enable tooltips everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
